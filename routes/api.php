3d80e3a94d34239bd709ff862205286e4b6a85e9
israel <israel16@hotmail.es>
2025-03-19 12:20:40 +0100

    Customizacion de rutas login, register y categorias (publicas), logout, ruta rol cliente, rutas rol admin gestion empresa, user, categoria y creacion controladores en arbol de directorio Api para Admin y Client

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,40 @@
 <?php
 
+use App\Http\Controllers\Api\Admin\CategoriaController;
+use App\Http\Controllers\Api\Admin\EmpresaController;
+use App\Http\Controllers\Api\Admin\UserController;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\Client\EmpresaController as EmpresaClient;
+use App\Http\Controllers\Api\FrontController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+
+Route::prefix('v1')->group(function (){
+    //Rutas publicas (No necesitan autenticacion)
+    //::public
+    Route::get('/public/{slug}', [FrontController::class, 'categoria']);
+    //::auth
+    Route::get('/auth/register', [AuthController::class, 'register']);
+    Route::get('/auth/login', [AuthController::class, 'login']);
+
+   
+   
+    //Rutas privadas
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+    //::rol cliente
+    Route::apiResource('/client/empresa', EmpresaClient::class)->middleware('auth:sanctum');
+
+    //::rol admin
+    Route::apiResource('/admin/empresa', EmpresaController::class)->middleware('auth:sanctum');
+    Route::apiResource('/admin/user', UserController::class)->middleware('auth:sanctum');
+    Route::apiResource('/admin/categoria', CategoriaController::class)->middleware('auth:sanctum');
+
+});
+
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
