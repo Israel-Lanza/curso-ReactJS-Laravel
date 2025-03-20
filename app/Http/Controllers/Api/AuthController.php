fdf5c503d5dd868e4ec9a245acdb52f95dfb0d39
israel <israel16@hotmail.es>
2025-03-20 16:01:30 +0100

    creacion del metodo logout, destruccion de tokens de usuarios logeados, retorno de respuesta y testeo con Thunder client

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -78,4 +78,17 @@ class AuthController extends Controller
 
 
     }
+
+    public function logout()
+    {
+        $response = ["success"=>false];
+        auth()->user->tokens()->delete();
+        $response = [
+            "success" => true,
+            "message" => "Sesión cerrada"
+        ];
+        return response()->json($response, 200);
+
+    }
+
 }
